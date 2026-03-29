949e009748d02f3676f7d27f5b1128eb10d65844
brainsynder <briansnyder498@gmail.com>
2026-03-28 21:32:28 -0400

    chore: replace javax.annotation.Nullable with org.jetbrains.annotations.Nullable in BiOptional.java

--- src/main/java/org/bsdevelopment/pluginutils/storage/optional/BiOptional.java
+++ src/main/java/org/bsdevelopment/pluginutils/storage/optional/BiOptional.java
@@ -1,6 +1,7 @@
 package org.bsdevelopment.pluginutils.storage.optional;
 
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
